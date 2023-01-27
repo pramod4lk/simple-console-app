75150090a517753e8d633029cf2d165eda2a8979
Pramod Chanaka Weerasinghe Kulathunga <pramod@pidm.local>
2023-01-27 15:27:17 +0800

    initial commit

---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var connectionString = configuration.GetConnectionString("StorageAccount");
+
+            string containerName = "textfiles";
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+            container.CreateIfNotExists();
+
+            string blobName = "sampletextfile";
+            string fileName = "sampletextfile.txt";
+            BlobClient blobClient = container.GetBlobClient(blobName);
+            blobClient.Upload(fileName, true);
+
+            var blobs = container.GetBlobs();
+            foreach (var blob in blobs)
+            {
+                Console.WriteLine($"{blob.Name} --> Created On: {blob.Properties.CreatedOn:yyyy-MM-dd HH:mm:ss}  Size: {blob.Properties.ContentLength}");
+            }
+        }
+    }
+}
